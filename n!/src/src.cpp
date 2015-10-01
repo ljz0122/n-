a867a8d36d6d906c5eb7ab192388c57ceebb5339
ericljz <ericljz@hotmail.com>
2015-10-01 22:00:57 +0800

    update

--- n!/src/src.cpp
+++ n!/src/src.cpp
@@ -1,9 +1,19 @@
 //============================================================================
-// Name        : 1.cpp
+// Name        : src.cpp
 // Author      : eric
 // Version     : 1.0.0
-// Copyright   : GNUv3
-// Description : Hello World in C++, Ansi-style
+// Copyright   : GNUv2
+// Description : compute n!
 //============================================================================
 
 #include<iostream>
+int main(){
+	long n,n1,Temp;
+	cin>>n1;
+	Temp=0;
+	for(n=1;n<=n1;n++){
+		Temp=n*Temp;
+	}
+	cout<<Temp<<endl;
+	return 0;
+}
